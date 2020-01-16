2dc57a5b09f05cf0b838f17d563a62c647e43512
angre-kim <angre@daum.net>
2020-01-16 16:20:57 +0900

    127강-BMI에러 이유

--- lib/calculating_brain.dart
+++ lib/calculating_brain.dart
@@ -6,6 +6,7 @@ class CalculatorBrain {
   final int height;
   final int weight;
 
+
   String calculateBMI() {    //메소드 만들기
     double bmi = weight / pow(height / 100, 2);
     bmi.toStringAsFixed(1);
@@ -21,3 +22,5 @@ class CalculatorBrain {
     }
   }
 }
+
+// bmi가 에러 나는 이유는 'String calculateBMI()'에서만 사용되고 'String getResult()'에서는 사용될 수 없으므로. 따라서 private로 만들어야주어야 한다.
