c571c2139db16b45dedaa568f496f55ab1de86a5
angre-kim <angre@daum.net>
2020-01-16 16:25:30 +0900

    127강-_BMI_해설 추가

--- lib/calculating_brain.dart
+++ lib/calculating_brain.dart
@@ -22,6 +22,14 @@ class CalculatorBrain {
       return '저체중';
     }
   }
+  String getInterpretation(){
+    if (_bmi >= 25) {
+      return '살빼라';
+    } else if (_bmi > 18.5) {
+      return '딱좋네';
+    } else {
+      return '마니 묵어라';
+    }
+  }
 }
 
-// bmi가 에러 나는 이유는 'String calculateBMI()'에서만 사용되고 'String getResult()'에서는 사용될 수 없으므로. 따라서 private로 만들어야주어야 한다.
