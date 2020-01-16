388b01ee02bde991160936213d6c85fc0de1ba64
angre-kim <angre@daum.net>
2020-01-16 16:22:40 +0900

    127강-_BMI 해주기

--- lib/calculating_brain.dart
+++ lib/calculating_brain.dart
@@ -6,16 +6,17 @@ class CalculatorBrain {
   final int height;
   final int weight;
 
+  double _bmi;
 
   String calculateBMI() {    //메소드 만들기
-    double bmi = weight / pow(height / 100, 2);
-    bmi.toStringAsFixed(1);
+    double _bmi = weight / pow(height / 100, 2);
+    _bmi.toStringAsFixed(1);
   }
 
   String getResult() {     //메소드 만들기
-    if (bmi >= 25) {
+    if (_bmi >= 25) {
       return '과체중';
-    } else if (bmi > 18.5) {
+    } else if (_bmi > 18.5) {
       return '정상';
     } else {
       return '저체중';
