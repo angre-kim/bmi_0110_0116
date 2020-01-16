3d42ea1460d5b8c102af8220d9fd2d7c4e8de204
angre-kim <angre@daum.net>
2020-01-16 16:17:59 +0900

    127강-BMI결과값 디스플레이 법칙 만들기

--- lib/calculating_brain.dart
+++ lib/calculating_brain.dart
@@ -1,13 +1,23 @@
 import 'dart:math';
+
 class CalculatorBrain {
   CalculatorBrain({this.height, this.weight});
 
   final int height;
   final int weight;
 
-  String calculateBMI(){                              //메소드
-    double bmi = weight / pow(height/ 100, 2);
+  String calculateBMI() {    //메소드 만들기
+    double bmi = weight / pow(height / 100, 2);
     bmi.toStringAsFixed(1);
   }
 
+  String getResult() {     //메소드 만들기
+    if (bmi >= 25) {
+      return '과체중';
+    } else if (bmi > 18.5) {
+      return '정상';
+    } else {
+      return '저체중';
+    }
+  }
 }
