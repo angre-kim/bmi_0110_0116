66376bb348b5d55eb08546eb62ac537dddd04b11
angre-kim <angre@daum.net>
2020-01-16 15:17:22 +0900

    126강-result page '재계산하기'버튼

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -205,27 +205,40 @@ class _InputPageState extends State<InputPage> {
                 ),
               ],
             )),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ResultsPage()));
-              },
-              child: Container(
-                child: Center(
-                  child: Text(
-                    '계산하기',
-                    style: kLargeButtonTextStyle,
-                  ),
-                ),
-                color: kBottomContainerColour,
-                margin: EdgeInsets.only(top: 10.0),
-                padding: EdgeInsets.only(bottom: 20.0),
-                width: double.infinity,
-                height: kBottomContainerHeight,
-              ),
-            )
+            ButtomButton()
           ],
         ));
+  }
+}
+
+class ButtomButton extends StatelessWidget {
+
+  ButtomButton({@required this.onTap, @required this.buttonTitle});
+
+final Function onTap;
+final String buttonTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => ResultsPage()));
+      },
+      child: Container(
+        child: Center(
+          child: Text(
+            '계산하기',
+            style: kLargeButtonTextStyle,
+          ),
+        ),
+        color: kBottomContainerColour,
+        margin: EdgeInsets.only(top: 10.0),
+        padding: EdgeInsets.only(bottom: 20.0),
+        width: double.infinity,
+        height: kBottomContainerHeight,
+      ),
+    );
   }
 }
 
