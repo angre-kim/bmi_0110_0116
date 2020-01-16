80a51a06cb2294e8ddec7ec922758d967f13c732
angre-kim <angre@daum.net>
2020-01-16 16:11:27 +0900

    127강-BMI Calculator Functionality

---
+++ lib/calculating_brain.dart
@@ -0,0 +1,12 @@
+import 'dart:math';
+class CalculatorBrain {
+  CalculatorBrain({this.height, this.weight});
+
+  final int height;
+  final int weight;
+
+  String calculateBMI(){
+    double bmi = weight / pow(height/ 100, 2);
+  }
+
+}
