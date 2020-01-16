7f34fca7a04c5069637689ae38615f75d9cf769b
angre-kim <angre@daum.net>
2020-01-16 13:24:23 +0900

    resultpage 만들기

---
+++ lib/result_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ResultsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI계산기 결과화면'),
+        ),
+      body: Text('결과페이지'),
+    );
+  }
+}
